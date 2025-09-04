f9b56eddd017ced78e6f501c61464f5565794711
Darshan <darshanpatel2923@gmail.com>
2025-09-04 23:06:48 +0530

    Added url downloader and custom django command

--- django/dave_gray_tuts/saas-foundations-project/src/cfehome/settings.py
+++ django/dave_gray_tuts/saas-foundations-project/src/cfehome/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'visits',
+    'commando',
 ]
 
 MIDDLEWARE = [
@@ -138,6 +139,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+
+STATICFILES_BASE_DIR.mkdir(parents=True, exist_ok=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendor"
+
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
