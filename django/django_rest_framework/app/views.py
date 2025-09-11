f2a8d1b5d71ff8790997661d1d23712632becc04
Darshan <darshanpatel2923@gmail.com>
2025-09-11 23:11:36 +0530

    Started with django rest framework

---
+++ django/django_rest_framework/app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Drinks
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+@api_view(['GET','POST'])
+def drink_list(request):
+    if request.method=='GET':
+        drinks=Drinks.objects.all()
+        serializer=DrinkSerializer(drinks,many=True)
+        return JsonResponse({"drinks":serializer.data})
+    
+    if request.method=='POST':
+        serializer=DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        
+
+@api_view(['GET','PUT','DELETE'])
+def drink_detail(request,id):
+    try:
+        drink=Drinks.objects.all()
+    except Drinks.DrinkDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET':
+        serializer=DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method=='PUT':
+        serializer=DrinkSerializer(drink,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method=='DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
