f6b3f065485bf99bc7865ccfe82f01cf6312b095
Darshan <darshanpatel2923@gmail.com>
2025-11-21 08:56:51 +0530

    Started with 100 days of python code challenge

---
+++ 100_Days_Python/Day1/generateBandName.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+# new line to get multi line output
+print("Hi\nThere")
+
+# combining strings
+print("Hello" + " " + "Python")
+
+# inputs
+# input("A prompt for the user")
+print("Hello " + input("What is your Name?\n"))
